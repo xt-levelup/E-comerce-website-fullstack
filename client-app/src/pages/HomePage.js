66e39c7265de7506799ccc8aa4241127d053221f
Phan Minh Cuong <levelup379452850@gmail.com>
2024-05-24 22:31:41 +0700

    Updated home page, product model in server

--- client-app/src/pages/HomePage.js
+++ client-app/src/pages/HomePage.js
@@ -1,14 +1,18 @@
 import { Helmet } from "react-helmet";
 import styles from "./HomePage.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import iphone from "../images/product_1.png";
 import mac from "../images/product_2.png";
 import ipad from "../images/product_3.png";
 import watch from "../images/product_4.png";
 import airpod from "../images/product_5.png";
+import adminAvatar from "../images/avatar01.png";
 
 const HomePage = () => {
   const navigate = useNavigate();
+
+  const [boxShow, setBoxShow] = useState(false);
 
   const browseCollectionButton = () => {
     navigate("/shop");
@@ -17,6 +21,27 @@ const HomePage = () => {
   const imageNavigation = () => {
     navigate("/shop");
   };
+
+  const messageBoxHandle = () => {
+    setBoxShow(!boxShow);
+  };
+
+  const boxShowToFalse = (event) => {
+    if (event.key === "Escape") {
+      setBoxShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", boxShowToFalse);
+    return () => {
+      window.removeEventListener("keydown", boxShowToFalse);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("boxShow:", boxShow);
+  }, [boxShow]);
 
   return (
     <div>
@@ -95,7 +120,7 @@ const HomePage = () => {
           <button>Subscribe</button>
         </div>
       </div>
-      <div className={styles["chat-icon"]}>
+      <div className={styles["chat-icon"]} onClick={messageBoxHandle}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -111,31 +136,70 @@ const HomePage = () => {
           />
         </svg>
       </div>
-      <div className={styles["chat-window"]}>
-        <div className={styles.header}>
-          <h3>Customer Support</h3>
-          <button>Let's Chat App</button>
-        </div>
-        <hr />
-        <div>
-          <div className={styles.client}>
-            <p>Xin chao</p>
-            <p>Lam the nao de xem san pham</p>
+      {boxShow && (
+        <div className={styles["chat-window-contain"]}>
+          <div className={styles["chat-window"]}>
+            <div className={styles.header}>
+              <h3>Customer Support</h3>
+              <button>Let's Chat App</button>
+            </div>
+            <div className={styles.chatting}>
+              <div className={styles.client}>
+                <p>Xin chao</p>
+                <p>Lam the nao de xem san pham</p>
+              </div>
+              <div className={styles.admin}>
+                <p>Chao ban</p>
+                <p>Ban co the vao muc Shop</p>
+              </div>
+            </div>
+            <div className={styles["chat-input"]}>
+              <img
+                src={adminAvatar}
+                style={{ width: "36px", height: "36px", marginLeft: "1em" }}
+              />
+              <input type="text" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 0 0-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634Zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 0 1-.189-.866c0-.298.059-.605.189-.866Zm2.023 6.828a.75.75 0 1 0-1.06-1.06 3.75 3.75 0 0 1-5.304 0 .75.75 0 0 0-1.06 1.06 5.25 5.25 0 0 0 7.424 0Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <svg
+                viewBox="0 0 48 48"
+                // fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M41.4193 7.30899C41.4193 7.30899 45.3046 5.79399 44.9808 9.47328C44.8729 10.9883 43.9016 16.2908 43.1461 22.0262L40.5559 39.0159C40.5559 39.0159 40.3401 41.5048 38.3974 41.9377C36.4547 42.3705 33.5408 40.4227 33.0011 39.9898C32.5694 39.6652 24.9068 34.7955 22.2086 32.4148C21.4531 31.7655 20.5897 30.4669 22.3165 28.9519L33.6487 18.1305C34.9438 16.8319 36.2389 13.8019 30.8426 17.4812L15.7331 27.7616C15.7331 27.7616 14.0063 28.8437 10.7686 27.8698L3.75342 25.7055C3.75342 25.7055 1.16321 24.0823 5.58815 22.459C16.3807 17.3729 29.6555 12.1786 41.4193 7.30899Z"
+                  fill="blue"
+                />
+              </svg>
+            </div>
           </div>
-          <div className={styles.admin}>
-            <p>Chao ban</p>
-            <p>Ban co the vao muc Shop</p>
-          </div>
         </div>
-        <hr />
-        <div className={styles["chat-input"]}>
-          <p>Icon</p>
-          <input type="text" />
-          <p>icon file</p>
-          <p>icon emoji</p>
-          <p>icon telegram</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
