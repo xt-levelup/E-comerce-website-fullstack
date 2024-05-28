4a109312c49b4944715f62b4fa18b9b039420eaa
Phan Minh Cuong <levelup379452850@gmail.com>
2024-05-28 09:25:11 +0700

    Updated serve/controllers/admin.js

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 
 exports.addProduct = (req, res, next) => {
   const name = req.body.name;
@@ -8,17 +9,25 @@ exports.addProduct = (req, res, next) => {
   const longDesc = req.body.longDesc;
   const imageFiles = req.files;
   const authHeader = req.get("Authorization");
-  const imageFilePaths = imageFiles.map((file) => {
-    return file.path;
-  });
+  const errors = validationResult(req);
+
+  // const imageFilePaths = imageFiles.map((file) => {
+  //   return file.path;
+  // });
 
   console.log("name:", name);
   console.log("price:", price);
   console.log("category:", category);
   console.log("shortDesc:", shortDesc);
   console.log("longDesc:", longDesc);
-  console.log("imageFilePaths:", imageFilePaths);
+  // console.log("imageFilePaths:", imageFilePaths);
   console.log("authHeader:", authHeader);
+  console.log("errors:", errors);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json(errors.array()[0]);
+    return;
+  }
 
   res.status(201).json({
     message: "Upload OK!",
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { check, body } = require("express-validator");
 
 const adminControllers = require("../controllers/admin");
 
-router.post("/addProduct", adminControllers.addProduct);
+router.post(
+  "/addProduct",
+  [
+    body(
+      "name",
+      "Please enter an Product Name with text or number!"
+    ).isAlphanumeric(),
+  ],
+  adminControllers.addProduct
+);
 
 module.exports = router;
